1bd5ecf0e154fa6c97d182d715fda664b63e3f35
Mark Cho <markcho@live.ca>
2013-05-19 06:28:40 -0400

    Changed the schema for User so nullable data can be inserted.

--- src/server/conf/evolutions/default/1.sql
+++ src/server/conf/evolutions/default/1.sql
@@ -2,9 +2,9 @@
 CREATE TABLE User(
   userId BIGINT NOT NULL AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  firstName VARCHAR(255) NOT NULL,
-  lastName VARCHAR(255) NOT NULL,
+  password VARCHAR(255),
+  firstName VARCHAR(255),
+  lastName VARCHAR(255),
   createdDate DATETIME NOT NULL,
   joinedDate DATETIME,
 
