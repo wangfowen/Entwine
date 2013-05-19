38a91cf6e2298048a8291c4348879123a8a5d709
Owen Wang <wangfowen@gmail.com>
2013-05-18 21:50:54 -0400

    fixed up form on index

--- src/server/app/controllers/Application.scala
+++ src/server/app/controllers/Application.scala
@@ -68,34 +68,4 @@ object Application extends Controller {
         }
     )
   }
-
-  def subscribe(_email: String) = Action { implicit request =>
-    try {
-      var email = _email
-      if (email == null)
-        email = subscribeForm.bindFromRequest.get
-
-      try {
-        /*
-
-        TODO: val subscriber = AccountsAPI.createSubscriber(email)
-
-        Global.automailerActor.tell(
-          EmailMsg(
-            subscriber.getEmailAccount.getEmail(),
-            "",
-            ""
-          )
-        )
-
-      */
-        Redirect(controllers.routes.Login.index(email))
-      }
-      catch {
-        case e: IllegalArgumentException =>
-          BadRequest(views.html.index(Some(e.getMessage())))
-      }
-    }
-  }
-
 }
