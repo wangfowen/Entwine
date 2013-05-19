6d61a103c6bce567bf2c78350e97d1d880eb7786
Mark Cho <markcho@live.ca>
2013-05-19 03:17:00 -0400

    Fixed compilation issue.

--- src/server/app/json/JsonResponse.scala
+++ src/server/app/json/JsonResponse.scala
@@ -1,9 +1,16 @@
 package json
 
 import models._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsNumber, JsValue, Writes, Json}
 
 object JsonResponse {
+  implicit val participationStatusEnum = new Writes[Participation.Status.Value] {
+    def writes(enum: Participation.Status.Value): JsValue = JsNumber(enum.id)
+  }
+  implicit val participationRoleEnum = new Writes[Participation.Role.Value] {
+    def writes(enum: Participation.Role.Value): JsValue = JsNumber(enum.id)
+  }
+
   implicit val user = Json.writes[User]
   implicit val contact = Json.writes[Contact]
   implicit val event = Json.writes[Event]
