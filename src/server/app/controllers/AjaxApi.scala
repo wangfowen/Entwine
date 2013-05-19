35580d4aabbd563c67112177091210dbbfa8860f
Mark Cho <markcho@live.ca>
2013-05-19 03:13:46 -0400

    Added the register and login ajax api.

--- src/server/app/controllers/AjaxApi.scala
+++ src/server/app/controllers/AjaxApi.scala
@@ -1,5 +1,32 @@
 package controllers
 
-object AjaxApi {
+import models._
+import json.JsonRequest
+import json.JsonResponse
+import play.api.Logger
+import play.api.libs.json._
+import play.api.mvc._
 
+object AjaxApi extends Controller with Authentication {
+  private implicit val userWrite = JsonResponse.user
+  private implicit val contactWrite = JsonResponse.contact
+  private implicit val eventWrite = JsonResponse.event
+  private implicit val participationWrite = JsonResponse.participation
+  private implicit val timeBlockWrite = JsonResponse.timeBlock
+
+  def register = Action(BodyParsers.parse.json) { request =>
+    request.body.validate[JsonRequest.Register].map { parsed =>
+      User.register(parsed.email, parsed.password, parsed.firstName, parsed.lastName).map { userId =>
+        Created(Json.obj("userId" -> userId))
+      }.getOrElse(BadRequest)
+    }.getOrElse(BadRequest)
+  }
+
+  def login = Action(BodyParsers.parse.json) { request =>
+    request.body.validate[JsonRequest.Login].map { parsed =>
+      User.authenticate(parsed.email, parsed.password).map { user =>
+        Ok(Json.toJson(user)).withSession("userId" -> user.userId.toString)
+      }.getOrElse(BadRequest)
+    }.getOrElse(BadRequest)
+  }
 }
--- src/server/app/json/JsonResponse.scala
+++ src/server/app/json/JsonResponse.scala
@@ -1,7 +1,12 @@
 package json
 
+import models._
 import play.api.libs.json.Json
 
 object JsonResponse {
-
+  implicit val user = Json.writes[User]
+  implicit val contact = Json.writes[Contact]
+  implicit val event = Json.writes[Event]
+  implicit val participation = Json.writes[Participation]
+  implicit val timeBlock = Json.writes[TimeBlock]
 }
