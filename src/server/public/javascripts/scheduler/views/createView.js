3bcebae4d4046fbfac354709e16b3284e099e957
Owen Wang <wangfowen@gmail.com>
2013-05-19 03:42:05 -0400

    create page

--- src/server/public/javascripts/scheduler/views/createView.js
+++ src/server/public/javascripts/scheduler/views/createView.js
@@ -3,6 +3,56 @@ Entwine.scheduler.views.CreateEventsView = Backbone.View.extend({
   cutoffDatePicker: null,
   submitButton: null,
   mainForm: null,
+  input: null,
+
+  createInvitee: function() {
+    var _this = this,
+        email = _this.input.val(),
+        invitee = "<p class='invitee'>" +
+            "<span class='email'>" + email.substring(0, email.length -1) + "</span>" +
+            "<span class='hover-buttons'>" +
+            "<label class='important'>" +
+              "<input type='checkbox' value='1'>" +
+              "<span class='star'>★</span>" +
+            "</label>" +
+            "<a href='#' class='close'>✖</a>" +
+            "</span>" +
+          "</p>";
+    _this.inviteeField.append(invitee);
+
+    var $invitee = _this.inviteeField.children().last(),
+        $hoverButtons = $invitee.children('.hover-buttons'),
+        $close = $hoverButtons.children('.close'),
+        $important = $hoverButtons.children('.important'),
+        green = "#14BC68",
+        hovered = "#dedede",
+        selected = "#FFC87C",
+        darkGreen = "#7AA129",
+        darkSelected = "#F79E60",
+        darkHovered = "#ccc";
+
+    $invitee.hover(function(e) {
+      $invitee.css("background", hovered);
+      $invitee.css("border-color", darkHovered);
+      $hoverButtons.fadeIn("fast");
+    }, function(e) {
+      if (!$important.children('input').prop("checked")) {
+        $invitee.css("background", green);
+        $invitee.css("border-color", darkGreen);
+      } else {
+        $invitee.css("background", selected);
+        $invitee.css("border-color", darkSelected);
+      }
+      $hoverButtons.hide();
+    });
+
+    $close.click(function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      $invitee.remove();
+    });
+  },
 
   initialize: function(){
     var _this = this;
@@ -11,18 +61,28 @@ Entwine.scheduler.views.CreateEventsView = Backbone.View.extend({
     this.cutoffDatePicker = $("#cutoff");
     this.submitButton = $("#submitButton");
     this.mainForm = $("#mainForm");
+    this.input = $('#input-invitee');
 
     this.render();
   },
 
   render: function(){
-    this.inviteeField.tagsInput({
-      width: "",
-      minInputWidth: "100%",
-      placeholderColor: "",
-      delimiter: ", ",
-      defaultText: "Add emails, separated by commas",
-      containerClass: "form-input field-textarea"
+    var _this = this;
+
+    _this.input.keyup(function(e) {
+      if (e.keyCode === 188 || e.keyCode === 32) {
+        if (_this.input.val().length > 1) {
+          _this.createInvitee();
+        }
+
+        _this.input.val('');
+      }
+    });
+
+    _this.input.keydown(function(e) {
+      if (e.keyCode === 8 && _this.input.val() === "") {
+        $('.invitee').last().remove();
+      }
     });
 
     this.cutoffDatePicker.datepicker({
